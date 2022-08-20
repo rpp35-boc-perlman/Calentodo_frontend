01c0bb204d117874f8736b35e4f73dd8b88ada02
Jon Personal <jtang312@gmail.com>
2022-08-20 17:23:54 -0400

    functioning framework

---
+++ client/src/components/Todo/sampleData.js
@@ -0,0 +1,39 @@
+let data = [
+  {
+    'description': 'todo1',
+    'start': 1661023689876,
+    'duration': 3600000,
+    'category': 'category1',
+    'status': 'late'
+  },
+  {
+    'description': 'todo2',
+    'start': 1661023689876,
+    'duration': 3600000,
+    'category': 'category2',
+    'status': 'late'
+  },
+  {
+    'description': 'todo3',
+    'start': 1661023689876,
+    'duration': 3600000,
+    'category': 'category1',
+    'status': 'pending'
+  },
+  {
+    'description': 'todo4',
+    'start': 1661023689876,
+    'duration': 3600000,
+    'category': 'category5',
+    'status': 'pending'
+  },
+  {
+    'description': 'todo6',
+    'start': 1661023689876,
+    'duration': 3600000,
+    'category': 'category2',
+    'status': 'pending'
+  }
+]
+
+export default data
