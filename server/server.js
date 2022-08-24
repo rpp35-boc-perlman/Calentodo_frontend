c41b3843086bf3bf4d4359b329ca89a9358f2c87
Chris Sampson <csamposn@gmail.com>
2022-08-23 23:06:11 -0400

    changing how files are delivered

--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const path = require('path')
+const morgan = require('morgan')
 
 const app = express();
 
-app.use(express.static('client/dist'));
+app.use(morgan('tiny'));
+
+const distDir = path.join(__dirname, '../client/dist')
+
+app.use( express.static(distDir) );
+
 
 // test router - only active during testing
 app.get('/status', (req,res,next) => {
