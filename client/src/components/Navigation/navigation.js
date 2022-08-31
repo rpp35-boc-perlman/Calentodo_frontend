c1462142f5c31faa92e6814a232581c1b92fb5de
Eli Veilleux <elibchv@gmail.com>
2022-08-31 13:54:54 -0700

    Mui drawer created

--- client/src/components/Navigation/navigation.js
+++ client/src/components/Navigation/navigation.js
@@ -1,14 +1,40 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
 import axios from 'axios';
-import Slide from '@mui/material/Slide';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function navigation() {
-  return (<div>
-    <nav className="navbar">
-      <Link className="link" to="/">Home</Link>
-      <Link className="link" to="/todo">Todos</Link>
-      <Link className="link" to="/statistics">Statistics</Link>
-    </nav>
+  const [state, setState] = React.useState({
+    left: false
+  });
+
+  const toggleDrawer = (open) => (e) => {
+    if (e.type === 'keydown' && (e.key === 'Tab' || e.key === 'Shift')) {
+      return;
+    }
+
+    setState({ left: open });
+  }
+
+  return (
+  <div>
+    <React.Fragment key="left">
+      <Button onClick={toggleDrawer(true)} variant="outlined" size="small">
+        <MenuIcon />
+      </Button>
+      <Drawer
+      anchor="left"
+      open={state.left}
+      onClose={toggleDrawer(false)}
+      >
+        <nav className="navbar">
+            <div className="nav-link"><Link to="/">Home</Link></div>
+            <div className="nav-link"><Link to="/todo">Todos</Link></div>
+            <div className="nav-link"><Link to="/statistics">Statistics</Link></div>
+        </nav>
+      </Drawer>
+    </React.Fragment>
   </div>)
 }
