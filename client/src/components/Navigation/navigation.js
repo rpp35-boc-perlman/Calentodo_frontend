4cf93303d4d45587e332c05931792e58be7099e3
Jon Personal <jtang312@gmail.com>
2022-08-30 23:49:53 -0400

    temporary navbar

--- client/src/components/Navigation/navigation.js
+++ client/src/components/Navigation/navigation.js
@@ -1,13 +1,68 @@
-import { Link } from 'react-router-dom';
-import React from 'react';
-import axios from 'axios';
+// import { Link } from 'react-router-dom';
+// import React from 'react';
+// import axios from 'axios';
 
-export default function navigation() {
-  return (<div>
-    <nav className="navbar">
-      <Link className="link" to="/">Home</Link>
-      <Link className="link" to="/todo">Todos</Link>
-      <Link className="link" to="/statistics">Statistics</Link>
-    </nav>
-  </div>)
+// export default function navigation() {
+//   return (<div>
+//     <nav className="navbar">
+//       <Link className="link" to="/">Home</Link>
+//       <Link className="link" to="/todo">Todos</Link>
+//       <Link className="link" to="/statistics">Statistics</Link>
+//     </nav>
+//   </div>)
+// }
+
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  CssBaseline,
+  Typography,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  let link = {
+    textDecoration: "none",
+    color: "white",
+    fontSize: "20px",
+    marginLeft: "20px",
+    "&:hover": {
+      color: "yellow",
+      borderBottom: "1px solid white",
+    },
+  };
+  
+  let logo = {
+    flexGrow: "1",
+    cursor: "pointer",
+  };
+
+  let navlinks = {
+    marginLeft: "10px",
+    display: "flex",
+  };
+
+  return (
+    <AppBar position="static">
+      <CssBaseline />
+      <Toolbar>
+        <Typography variant="h4" sx={logo}>
+          <Link to="/" style={link}>
+            Home
+          </Link>
+        </Typography>
+          <div >
+            <Link to="/todo" style={link}>
+              Todos
+            </Link>
+            <Link to="/statistics" style={link}>
+              Statistics
+            </Link>
+          </div>
+      </Toolbar>
+    </AppBar>
+  );
 }
+
+export default Navbar;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,7 +5,6 @@ import Navigation from '../components/Navigation/navigation.js'
 export default function Home (props) {
     return(
         <div>
-            <h1>Home</h1>
             <Navigation />
         </div>
     )
