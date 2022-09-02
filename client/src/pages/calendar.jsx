a5b6d1f9887fb55f0567dbc9e6517bd4450423ba
Jon Personal <jtang312@gmail.com>
2022-09-01 23:03:07 -0400

    adding navbar to calendar

--- client/src/pages/calendar.jsx
+++ client/src/pages/calendar.jsx
@@ -4,24 +4,29 @@ import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
+import Navigation from '../components/Navigation/navigation.js'
+
 const localizer = momentLocalizer(moment);
 
 const TodoCalendar = ({ todos }) => {
   return (
-    <Calendar
-      defaultDate={moment().toDate()}
-      defaultView="month"
-      localizer={localizer}
-      style={{ height: '100vh' }}
-      resizable
-      events={todos?.map((todo) => {
-        return {
-          start: todo.start_date,
-          end: todo.end_date,
-          title: todo.todo_body,
-        };
-      })}
-    />
+    <>
+      <Navigation />
+      <Calendar
+        defaultDate={moment().toDate()}
+        defaultView="month"
+        localizer={localizer}
+        style={{ height: '100vh' }}
+        resizable
+        events={todos?.map((todo) => {
+          return {
+            start: todo.start_date,
+            end: todo.end_date,
+            title: todo.todo_body,
+          };
+        })}
+      />
+    </>
   );
 };
 
