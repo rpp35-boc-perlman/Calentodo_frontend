c2b99223dd57e6f1ec36c4b8ee194345c78ce64f
Eli Veilleux <elibchv@gmail.com>
2022-09-01 20:04:13 -0700

    cleaned up chart display

--- client/src/components/Statistics/chartView.js
+++ client/src/components/Statistics/chartView.js
@@ -8,7 +8,7 @@ class ChartView extends React.Component {
     super(props);
     this.state = {
       labels: [],
-      data: []
+      data: [{backgroundColor:'rgb(66, 135, 245)', data: 0, label: ''}]
     }
     this.dataMake = this.dataMake.bind(this);
   }
@@ -18,38 +18,42 @@ class ChartView extends React.Component {
       //iterate through props and get the body for each todo
       //these are the labels
       if (this.props.activeTodos.length === 0) {
-        return;
-      }
-      var labels = [];
-      var data = {
-        label: General, //category
-        backgroundColor: [],
-        data: []
-      };
+        this.setState({
+          labels: [],
+          data: [{backgroundColor:'rgb(66, 135, 245)', data: 0, label: ''}]
+        })
+      } else {
+        var labels = [];
+        var data = {
+          label: 'General', //category
+          backgroundColor: [],
+          data: []
+        };
 
-      for (var i = 0; i < this.props.activeTodos.length; i++) {
-        //get start and end date from todo
-        var start = new Date(this.props.activeTodos[i].start_date).getTime();
-        var end = new Date(this.props.activeTodos[i].end_date).getTime();
-        data.data.push(Math.round(Math.abs(end - start) / 3600000))
-        if (this.props.activeTodos[i].color) {
-          data.backgroundColor.push(this.props.activeTodos[i].color)
-        } else {
-          data.backgroundColor.push('rgb(66, 135, 245)')
+        for (var i = 0; i < this.props.activeTodos.length; i++) {
+          //get start and end date from todo
+          var start = new Date(this.props.activeTodos[i].start_date).getTime();
+          var end = new Date(this.props.activeTodos[i].end_date).getTime();
+          data.data.push(Math.round(Math.abs(end - start) / 3600000))
+          if (this.props.activeTodos[i].color) {
+            data.backgroundColor.push(this.props.activeTodos[i].color)
+          } else {
+            data.backgroundColor.push('rgb(66, 135, 245)')
+          }
+          if (this.props.activeTodos[i].category) {
+            data.label = this.props.activeTodos[i].category;
+          }
+          //convert to unix epoch
+          //calculate difference in miliseconds
+          //convert to hours
+          labels.push(this.props.activeTodos[i].todo_body);
         }
-        if (this.props.activeTodos[i].category) {
-          data.label = this.props.activeTodos[i].category;
-        }
-        //convert to unix epoch
-        //calculate difference in miliseconds
-        //convert to hours
-        labels.push(this.props.activeTodos[i].todo_body);
-      }
 
-      this.setState({
-        labels: labels,
-        data: data
-      });
+        this.setState({
+          labels: labels,
+          data: data
+        });
+      }
   }
 
   componentDidMount() {
