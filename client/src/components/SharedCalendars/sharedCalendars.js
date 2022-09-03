8efe321f096fd2b6a850921c29385af78601c3d5
Daniel Weiner <daweiner16@gmail.com>
2022-09-03 15:13:05 -0400

    get data from server

--- client/src/components/SharedCalendars/sharedCalendars.js
+++ client/src/components/SharedCalendars/sharedCalendars.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import CalendarList from './calendarList';
-
+import axios from 'axios';
 
 class SharedCalendars extends React.Component {
   constructor(props) {
@@ -9,43 +9,43 @@ class SharedCalendars extends React.Component {
     this.state = {
 
       users: [
-        {
-          "user_id": 9,
-          "user_email": "tyrone@test.com",
-          "todos": [
-            {
-              "todo_id": 3,
-              "start_date": "2001-08-27T00:00:00.000Z",
-              "end_date": "2001-08-29T00:00:00.000Z",
-              "todo_body": "Think about life",
-              "category": null,
-              "status": "pending",
-              "user_id": 9
-            },
-            {
-              "todo_id": 2,
-              "start_date": "2001-08-25T00:00:00.000Z",
-              "end_date": "2001-08-28T00:00:00.000Z",
-              "todo_body": "GO VACUUM",
-              "category": null,
-              "status": "pending",
-              "user_id": 9
-            },
-            {
-              "todo_id": 4,
-              "start_date": "2022-08-27T20:00:00.000Z",
-              "end_date": "2022-08-27T22:00:00.000Z",
-              "todo_body": "Movie Night",
-              "category": null,
-              "status": "pending",
-              "user_id": 9
-            }
-          ]
-        },
-        {
-          "user_id": 13,
-          "user_email": "me@test.com"
-        }
+        // {
+        //   "user_id": 9,
+        //   "user_email": "tyrone@test.com",
+        //   "todos": [
+        //     {
+        //       "todo_id": 3,
+        //       "start_date": "2001-08-27T00:00:00.000Z",
+        //       "end_date": "2001-08-29T00:00:00.000Z",
+        //       "todo_body": "Think about life",
+        //       "category": null,
+        //       "status": "pending",
+        //       "user_id": 9
+        //     },
+        //     {
+        //       "todo_id": 2,
+        //       "start_date": "2001-08-25T00:00:00.000Z",
+        //       "end_date": "2001-08-28T00:00:00.000Z",
+        //       "todo_body": "GO VACUUM",
+        //       "category": null,
+        //       "status": "pending",
+        //       "user_id": 9
+        //     },
+        //     {
+        //       "todo_id": 4,
+        //       "start_date": "2022-08-27T20:00:00.000Z",
+        //       "end_date": "2022-08-27T22:00:00.000Z",
+        //       "todo_body": "Movie Night",
+        //       "category": null,
+        //       "status": "pending",
+        //       "user_id": 9
+        //     }
+        //   ]
+        // },
+        // {
+        //   "user_id": 13,
+        //   "user_email": "me@test.com"
+        // }
       ],
       visibleUsers: [],
       checkedState: []
@@ -55,6 +55,23 @@ class SharedCalendars extends React.Component {
   }
 
   componentDidMount() {
+    const config = {
+      url: '/api/',
+      method: 'get',
+      headers: {
+        target: 'http://ec2-34-205-69-211.compute-1.amazonaws.com/users/9'
+      }
+    }
+    axios(config)
+    .then(res => {
+      this.setState({
+        users: res.data.users
+      },() => console.log(this.state.users, 'users'))
+    })
+    .catch((err) => {
+      console.error(err)
+    }
+    )
     this.setState({
       checkedState: new Array(this.state.users.length).fill(false)
     })
