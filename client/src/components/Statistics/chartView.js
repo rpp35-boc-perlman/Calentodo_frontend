9bc0d7f3ed0f0a2f6569df3dca48ff5a3b4a9485
Eli Veilleux <elibchv@gmail.com>
2022-09-06 19:47:17 -0700

    chart view colors changed

--- client/src/components/Statistics/chartView.js
+++ client/src/components/Statistics/chartView.js
@@ -75,14 +75,34 @@ class ChartView extends React.Component {
 
   render() {
     return (
-      <Grid item xs={12} md={10} sx={{ backgroundColor: '#113255', padding: 3  }}>
-        <Bar id="statisticsChart" data={
+      <Grid item xs={12} md={10} sx={{ backgroundColor: '#113255', padding: 2,  color: '#ffffff', margin: 0.5 }}>
+        <Bar  id="statisticsChart" data={
           {
             labels: this.state.labels,
             datasets: [this.state.data]
           }
         }
-        options={{plugins:{legend:{ display: this.state.display, bodyColor: '#fff'}}}}
+        options={{
+          plugins: {
+            legend: {
+              labels: {
+                color: 'white'
+              }
+            }
+          },
+          scales: {
+            y: {
+              ticks: {
+                color: 'white'
+              }
+            },
+            x:{
+              ticks: {
+                color: 'white'
+              }
+            }
+          }
+        }}
         ></Bar>
       </Grid>
     )
--- client/src/index.js
+++ client/src/index.js
@@ -46,7 +46,7 @@ class App extends React.Component {
               <Route path="/login" element={<LoginPage />} />
               {/* <Route path="/todo" element={<Main />} /> */}
               <Route path="/calendar" element={<TodoCalendar />} />
-              <Route path="/statistics" element={<Statistics />} />
+              <Route path="/statistics" element={<StatisticsWrapper />} />
               <Route path="/sharedCalendars" element={<SharedCalendars />} />
             </Routes>
           </HashRouter>
