acdd85a26ecba1883c7952d4f48572f5f73bb144
Eli Veilleux <elibchv@gmail.com>
2022-09-08 19:17:46 -0700

    pull req edits

--- test/statistics.test.js
+++
@@ -1,6 +0,0 @@
-// import renderer from 'react-test-renderer';
-// import StatisticsWrapper from '../client/src/components/Statistics/statisticsWrapper';
-
-// it('renders properly', () => {
-//   shallow(<StatisticsWrapper />);
-// });
